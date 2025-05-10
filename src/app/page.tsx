3e2019fec0391fae2e6461465cc031b11e547f5b
Yash Gupta <yash@knot.dating>
2025-05-11 03:19:00 +0530

    header + header animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import HeaderNavbar from "@/components/header-navbar";
+import HeroSection from "@/components/hero/hero-section";
 import Logo from "@/components/logo";
 
 export default function Home() {
   return (
     <>
-      <header className="px-24 pt-8 flex items-center justify-between">
+      <header className="px-24 pt-8 h-fit flex items-center justify-between animate-slideDown">
         <Logo />
 
         <HeaderNavbar />
       </header>
+
+      <main className="bg-red-200 ">
+        <HeroSection />
+      </main>
     </>
   );
 }
---
+++ src/components/hero/hero-section.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div>
+      <p>Hey there! I&apos;am Yash Gupta</p>
+    </div>
+  );
+}
